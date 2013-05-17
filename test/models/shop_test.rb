fea12e6795918ac021901b3ad3e19b3825341cd7
Steve Klabnik <steve@steveklabnik.com>
2013-05-17 12:47:05 -0700

    refactor shop test to be more explicit.
    We want to make sure that we have all the required params, but we're only
    missing the ones we're testing for.

--- test/models/shop_test.rb
+++ test/models/shop_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class ShopTest < ActiveSupport::TestCase
+  let(:required_params) do
+    {
+      name: "A shop.",
+    }
+  end
+
   test "name is required" do
-    shop = Shop.new
+    required_params.delete(:name)
+
+    shop = Shop.new(required_params)
 
     assert !shop.save, "Saved the shop without a name"
   end
