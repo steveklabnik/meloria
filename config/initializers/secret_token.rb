5c8ccaaf2b34e0e856b4248312efa461e5a7a76d
Steve Klabnik <steve@steveklabnik.com>
2013-05-17 08:33:43 -0700

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meloria::Application.config.secret_key_base = '9c4ae81fbf2016d5a8effcaed368a453d28f5c6fbd7f6052905b48ff455f21302a38604051fc05ee91abe79834925f660e229d95cc8cb452acc70669364a5ea1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Meloria::Application.config.session_store :cookie_store, key: '_meloria_session'
