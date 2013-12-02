4428b3b457de31d3028a9d0626a48fb22446d579
Steve Klabnik <steve@steveklabnik.com>
2013-12-02 02:31:18 +0000

    Fix the select boxes

--- app/assets/javascripts/controllers/customers_new_controller.js
+++ app/assets/javascripts/controllers/customers_new_controller.js
@@ -1,4 +1,14 @@
 Meloria.CustomersNewController = Ember.ObjectController.extend({
+  locals: [
+    { name: "Local", value: 1},
+    { name: "Out of town", value: 0},
+  ],
+  statuses: [
+    { name: "Ongoing", value: 1},
+    { name: "Waiting list", value: 2},
+    { name: "Finished", value: 3},
+    { name: "On Hold", value: 4},
+  ],
   actions: {
     submit: function() {
       this.get('model').save().then(function(model) {
